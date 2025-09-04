a9bd522eff0e2ed79f5cf7f5a16d3e552675a8ea
HammadCB <hammadfarooqicb@gmail.com>
2025-09-04 17:58:44 +0500

    Parametrized Constructor

---
+++ CH_3/Para_Cnstructor.java
@@ -0,0 +1,26 @@
+package CH_3;
+
+class Car {
+    String brand;
+    int year;
+
+    // Parameterized Constructor
+    Car(String b, int y) {
+        brand = b;
+        year = y;
+    }
+
+    void show() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
+
+public class Para_Cnstructor {
+    public static void main(String[] args) {
+        Car c1 = new Car("Toyota", 2020);
+        Car c2 = new Car("Honda", 2022);
+
+        c1.show();
+        c2.show();
+    }
+}
