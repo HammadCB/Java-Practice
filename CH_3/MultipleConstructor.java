05cbc80093c5d2fa54ae4acf698d4532fc8e7dfd
HammadCB <hammadfarooqicb@gmail.com>
2025-09-04 18:23:52 +0500

    Multiple Constructor Overload

---
+++ CH_3/MultipleConstructor.java
@@ -0,0 +1,29 @@
+package CH_3;
+
+class Circle {
+    double radius;
+
+    // Default constructor
+    Circle() {
+        radius = 5.0; // default radius
+    }
+
+    // Parameterized constructor
+    Circle(double r) {
+        radius = r;
+    }
+
+    double area() {
+        return 3.14 * radius * radius;
+    }
+}
+
+public class MultipleConstructor {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();       // default constructor
+        Circle c2 = new Circle(7.5);    // parameterized constructor
+
+        System.out.println("Area (default): " + c1.area());
+        System.out.println("Area (given): " + c2.area());
+    }
+}
