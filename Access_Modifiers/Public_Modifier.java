4bf2bef810d63472ac58bdb35e58c8eab002d044
HammadCB <hammadfarooqicb@gmail.com>
2025-09-05 16:34:11 +0500

    Private Modifier use for making it secure

---
+++ Access_Modifiers/Public_Modifier.java
@@ -0,0 +1,30 @@
+package Access_Modifiers;
+
+class BankAccount {
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+
+
+public class Public_Modifier {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(1000);
+        account.withdraw(500);
+        System.out.println("Current Balance: " + account.getBalance());
+    }
+    
+}
