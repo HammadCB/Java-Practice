15a952524fcb2d5e3fcc574096da2cdbe81eef23
HammadCB <hammadfarooqicb@gmail.com>
2025-09-06 18:08:49 +0500

    Exceptions

---
+++ Exception_Handling/Exception.java
@@ -0,0 +1,19 @@
+package Exception_Handling;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Exception {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();   
+            System.out.println("You entered: " + num);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numbers only.");
+        }
+
+        System.out.println("Program continues normally...");
+    }
+}
