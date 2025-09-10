7048bc775b9b98bb1a20638c39aa38d151f87341
HammadCB <hammadfarooqicb@gmail.com>
2025-09-10 13:13:10 +0500

    Changed minor things

---
+++ Exception_Handling/trycatch.java
@@ -0,0 +1,20 @@
+package Exception_Handling;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class trycatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();   
+            System.out.println("You entered: " + num);
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numbers only.");
+        }
+
+        System.out.println("Program continues normally...");
+    }
+}
