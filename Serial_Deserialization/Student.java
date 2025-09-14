e84bff456d64bb89e6d1dab7d87b00f53f2e7f64
HammadCB <hammadfarooqicb@gmail.com>
2025-09-14 23:45:34 +0500

    Structure for files

---
+++ Serial_Deserialization/Student.java
@@ -0,0 +1,15 @@
+package Serial_Deserialization;
+
+import java.io.Serializable;
+
+
+class Student implements Serializable {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
