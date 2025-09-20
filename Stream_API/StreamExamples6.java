cca28f9e30710d09f9ed1457cd7329e1644a8d5f
HammadCB <hammadfarooqicb@gmail.com>
2025-09-20 14:47:24 +0500

    Steam .limit . skip

---
+++ Stream_API/StreamExamples6.java
@@ -0,0 +1,27 @@
+package Stream_API;
+
+import java.util.List;
+
+public class StreamExamples6 {
+    public static void main(String[] args) {
+        
+    
+List<Integer> nums = List.of(10, 20, 30, 40, 50);
+
+ nums.stream()
+    .limit(3)
+    .forEach(System.out::println);
+
+
+
+nums.stream()
+    .skip(2)
+    .forEach(System.out::println);
+
+
+
+
+
+
+    }
+}
